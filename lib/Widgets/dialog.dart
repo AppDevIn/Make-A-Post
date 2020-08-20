2057c84e873c4b4224bcea57aadf833be024aa24
AppDevIn <teamprojectlive@gmail.com>
2020-08-21 02:29:21 +0800

    Added name

--- lib/Widgets/dialog.dart
+++ lib/Widgets/dialog.dart
@@ -7,27 +7,24 @@ import 'package:image_cropper/image_cropper.dart';
 import 'listView.dart';
 import 'package:InstaPostOnly/class/post.dart';
 
-
-class CustomDialog extends StatefulWidget{
-
+class CustomDialog extends StatefulWidget {
   PostView update;
   //Defualt construtor
   CustomDialog({this.update});
 
   @override
   State<StatefulWidget> createState() => _CustomDialogState(update);
-
 }
 
-class _CustomDialogState extends State<CustomDialog>{
-
+class _CustomDialogState extends State<CustomDialog> {
   File _file;
   PostView update;
   final picker = new ImagePicker();
 
-  final TextEditingController _controller = new TextEditingController();
+  final TextEditingController _name = new TextEditingController();
+  final TextEditingController _caption = new TextEditingController();
 
-  Future<void> _pickImage(ImageSource source) async{
+  Future<void> _pickImage(ImageSource source) async {
     final selected = await picker.getImage(source: source);
 
     setState(() => _file = File(selected.path));
@@ -43,7 +40,6 @@ class _CustomDialogState extends State<CustomDialog>{
 
   @override
   Widget build(BuildContext context) {
-
     return Dialog(
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(16),
@@ -52,64 +48,68 @@ class _CustomDialogState extends State<CustomDialog>{
       backgroundColor: Colors.transparent,
       child: dialogContent(),
     );
-
   }
 
-  dialogContent(){
+  dialogContent() {
     return Container(
       padding: EdgeInsets.all(16),
       margin: EdgeInsets.only(top: 16),
       decoration: BoxDecoration(
-        color: Colors.white,
-        shape: BoxShape.rectangle,
-        borderRadius: BorderRadius.circular(17),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black26,
-            blurRadius: 10.0,
-            offset: Offset(0.0, 10.0)
-          ),
-        ]
-      ),
+          color: Colors.white,
+          shape: BoxShape.rectangle,
+          borderRadius: BorderRadius.circular(17),
+          boxShadow: [
+            BoxShadow(
+                color: Colors.black26,
+                blurRadius: 10.0,
+                offset: Offset(0.0, 10.0)),
+          ]),
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: <Widget>[
           Text("Make a post"),
           SizedBox(height: 16),
           TextField(
-            controller: _controller ,
+            controller: _name,
+            decoration: InputDecoration.collapsed(hintText: "Name"),
+          ),
+          SizedBox(height: 16),
+          TextField(
+            controller: _caption,
+            decoration: InputDecoration.collapsed(hintText: "Caption"),
           ),
           SizedBox(height: 16),
           Row(
-           children: <Widget>[
-             RaisedButton(
-               onPressed: () => _pickImage(ImageSource.gallery),
-               child: Text("Upload image",
-               style: TextStyle(
-                 color: Colors.white
-               ),
-               ), 
+            children: <Widget>[
+              RaisedButton(
+                onPressed: () => _pickImage(ImageSource.gallery),
+                child: Text(
+                  "Upload image",
+                  style: TextStyle(color: Colors.white),
+                ),
               ),
-                     RaisedButton(
-               onPressed: () async {
-                final bytes = await _file.readAsBytes();
-                print("clicked");
-                // String _base64 = base64Encode(response.bodyBytes);
-                // Uint8List bytes = base64Decode(_base64);
-                 update.addPost(new Post(name: "Test", imageCode: bytes ));
-               },
-               child: Text("Upload",
-               style: TextStyle(
-                 color: Colors.white
-               ),
-               ), 
+              SizedBox(
+                width: 16,
               ),
-           ], 
+              Expanded(
+                child: RaisedButton(
+                  onPressed: () async {
+                    final bytes = await _file.readAsBytes();
+                    print("clicked");
+                    // String _base64 = base64Encode(response.bodyBytes);
+                    // Uint8List bytes = base64Decode(_base64);
+                    update.addPost(new Post(name: _name.text, imageCode: bytes));
+                  },
+                  child: Text(
+                    "Upload",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+            ],
           )
-
         ],
       ),
     );
   }
-
 }
